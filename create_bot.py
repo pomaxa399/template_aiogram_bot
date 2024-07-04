0e75fee590f26ceaf53e1c15cc499b248433478f
pomaxa399 <pomaxa399@gmail.com>
2024-07-04 16:57:09 +0300

    create bot

---
+++ create_bot.py
@@ -0,0 +1,12 @@
+import logging
+from aiogram import Bot, Dispatcher
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
+from aiogram.fsm.storage.memory import MemoryStorage
+from core.config import settings
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+
+bot = Bot(token=settings.bot.token, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+dp = Dispatcher(storage=MemoryStorage())
